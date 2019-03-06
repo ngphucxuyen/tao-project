4a68c9969cacb558b75b5424156ff8dbad862ca6
phucxuyen <1551010153xuyen@ou.edu.vn>
2019-03-06 19:41:10 +0700

    123

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ namespace demo
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //comment tao-nhanh
         }
     }
 }
