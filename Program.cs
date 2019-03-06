9724612ff881b7cb709623fe5a1f2481322946f6
Quoc Khanh <kquoc887@gmai.com>
2019-03-06 19:26:06 +0700

    create comment in file program.cs

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ namespace demo
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            // Comment's Khanh
         }
     }
 }
